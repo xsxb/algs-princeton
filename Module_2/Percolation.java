f69142e2ef04e3704c43da27ca0a061c5eeb0eb7
xsxb <ruoyu100@163.com>
2024-10-15 08:54:20 -0400

    A bunch of stuff on Percolation

--- Module_2/Percolation.java
+++ Module_2/Percolation.java
@@ -1,41 +1,62 @@
 import edu.princeton.cs.algs4.StdRandom;
 
+//TODO: Keep track of trees and tree sizes
+//	Move main to PercolationStats
+
 public class Percolation{
 	
-	static int n;
-	static int T;
-	static int[][] grid; //0->closed; 1->open
-	static int size;
+	static int n;		//Grid size
+	static int[][] grid; 	//Matrix of sites/nodes: 0->closed; 1->open; 2->full
+	static int size;	//n*n
+	static int p;		//Site vacancy probability
+	int[][] gridId;		//Root IDs of sites/nodes
+	int[][] treeSize;	//Size of connected component tree structure
 
-	//init n-by-n grid, all sites blocked
+	//Initialize n-by-n grid, all sites blocked
 	public Percolation(int n) {
 		grid = new int[n][n];
+		gridId = new int[n][n];
+		
+		grid[0][0] = 1;		//Virtual top node
+		grid[n+1][n+1] = 1;	//Virtual bottom node
+		gridId[0][0] = 0;
+		gridId[n+1][n+1] = n+1;
+
+
+		int id = 1;
 		for (int i = 1; i <=n; i++) {
 			for (int j = 1; j <=n; j++) {
 				grid[i][j] = 0;
+				gridId[i][j] = id;
+				id++;
 			}
 		}
 	}
 
-	//open site (row, col)
+	//Open site (row, col)
 	public void open(int row, int col) {
 		grid[row][col] = 1;
 	}
 
-	//is the site (row, col) open?
+	//Is the site (row, col) open?
 	public boolean isOpen(int row, int col) {
+		//Error handling
 		if (0 <= row || row > n  || 0 <= col || col > n) {
 			throw new IllegalArgumentException();
 		}	
 		return (grid[row][col] == 1) ? true : false;
 	}
 
-	//is the site (row, col) full?
+	//Is the site (row, col) full?
 	public boolean isFull(int row, int col) {
-		return false;
+		//Error handling
+		if (0 <= row || row > n  || 0 <= col || col > n) {
+			throw new IllegalArgumentException();
+		}	
+		return (grid[row][col] == 1) ? true : false;
 	}
 
-	//return number of open sites
+	//Returns number of open sites
 	public int numberOfOpenSites() {
 		int count = 0;
 		for (int i = 1; i <= n; i++) {
@@ -51,41 +72,63 @@ public class Percolation{
 		return false;
 	}
 
-	//Returns n by n matrix grid[n][n]
-	private static int[][] initGrid(int n) {
-		grid = new int[n][n];
-		for (int i = 1; i <=n; i++) {
-			for (int j = 1; j <=n; j++) {
-				grid[i][j] = 0;
-			}
-		}
-		return grid;
+	//Returns a 4 row array value[row,col]: top, left, right, bottom neighbour
+	private int[][] getNeighbours(int row, int col) {
+		int[][] neighbours = {
+			{ grid[row-1][col], row-1, col },
+			{ grid[row][col-1], row, col-1 },
+			{ grid[row][col+1], row, col+1 }, 
+			{ grid[row+1][col], row+1, col }
+		};
+		return neighbours;
 	}
 
-	//test client
+	private void union(int[] source, int[] dest) {}
+
+	//Connect trees
+	private void connect(int row, int col) {
+		int[][] neighbours = getNeighbours(row, col);
+		for (int i = 0; i < neighbours.length; i++) {
+			if (neighbours[i][0]==1) {
+				int y = neighbours[i][1];
+				int x = neighbours[i][2];
+				int[] source = new int[2];
+				int[] dest = new int[2]; 
+				if (treeSize[row][col] > treeSize[y][x]) {
+				}
+				union(source, dest);
+			}
+		}
+	}
+
+	//Test client
 	public static void main(String[] args) {
 		
+		//Read arguments
 		n = Integer.parseInt(args[0]);
-		T = Integer.parseInt(args[1]);
-
-		if (n <= 0 || T <= 0) {
+		if (n <= 0) {
 			throw new IllegalArgumentException();
 		}
-
+	
+		//TODO: Move below code to PercolationStats
+		//Init percolation object
+		Percolation percolation = new Percolation(n);
+		grid = percolation.grid;
 		size = n*n;
-		grid = initGrid(n);
-		//open sites n times before checking, because can't percolate at < n
+
+		//Open sites n times before checking, because can't percolate at T < n
 		for (int i = 0; i < n; i++) {
 			int row = StdRandom.uniformInt(1, n);
 			int column = StdRandom.uniformInt(1, n);
-			open(row, column);
+			percolation.open(row, column);
 		}
 		
-		//open sites until percolates
-		while (!percolates()) {
+		//Open sites until percolates
+		while (!percolation.percolates()) {
 			int row = StdRandom.uniformInt(1, n);
 			int column = StdRandom.uniformInt(1, n);
-			open(row, column);
+			percolation.open(row, column);
+			//percolation.connect(row, column);
 		}
 	}
 }
